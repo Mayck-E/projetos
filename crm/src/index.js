4522c2021d58c170e85117035fc12e3e4031abb1
May_Edu <mayck22.03@hotmail.com>
2023-10-11 16:51:56 -0300

    Initial commit

---
+++ crm/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./style/global.css";
+import Dashboard from "./pages/dashboard.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Dashboard />
+
+);
+
